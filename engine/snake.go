f05e479b5dd29833ab641c4a818d7276992a3806
WilliamCSA04 <williamcaetano14@gmail.com>
2025-02-17 22:46:58 -0300

    :sparkles: Added size counter to snake

--- engine/snake.go
+++ engine/snake.go
@@ -4,6 +4,7 @@ type Snake struct {
 	Coords
 	width  int
 	height int
+	size   int
 }
 
 func SpawnSnake(x int, y int, width int, height int) *Snake {
@@ -13,10 +14,15 @@ func SpawnSnake(x int, y int, width int, height int) *Snake {
 		},
 		width,
 		height,
+		1,
 	}
 }
 
 func (s *Food) Move(x int, y int) {
 	s.x = x
 	s.y = y
+}
+
+func (s *Snake) Eat() {
+	s.size++
 }
