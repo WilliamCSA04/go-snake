aca3dfd00189f2373fb0bd7fa7ed09b7a919ae6f
WilliamCSA04 <williamcaetano14@gmail.com>
2025-02-18 21:17:50 -0300

    :recycle: Eat function

--- engine/snake.go
+++ engine/snake.go
@@ -1,30 +1,37 @@
 package engine
 
 type Snake struct {
-	x      []int
-	y      []int
-	width  int
-	height int
-	size   int
+	x         []int
+	y         []int
+	lastTailX int
+	lastTailY int
+	width     int
+	height    int
+	size      int
 }
 
 func SpawnSnake(startX int, startY int, width int, height int) *Snake {
 	x := append(make([]int, 0), startX)
 	y := append(make([]int, 0), startY)
 	return &Snake{
-		x,
-		y,
-		width,
-		height,
-		1,
+		x:         x,
+		y:         y,
+		lastTailX: startX,
+		lastTailY: startY,
+		width:     width,
+		height:    height,
+		size:      1,
 	}
 }
 
 func (s *Snake) Move(x int, y int) {
+	s.lastTailX = s.x[len(s.x)-1]
+	s.lastTailY = s.y[len(s.y)-1]
 	s.x = append([]int{x}, s.x[:len(s.x)-1]...)
 	s.y = append([]int{y}, s.y[:len(s.y)-1]...)
 }
 
 func (s *Snake) Eat() {
-	s.size++
+	s.x = append(s.x, s.lastTailX)
+	s.y = append(s.y, s.lastTailY)
 }
